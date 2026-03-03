cc51637922e1c7727098da31033fb41198e66f56
Petr Skoda <petr@mutms.org>
2026-03-03 10:00:09 +0100

    Let begin!

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+
+	site: 'https://www.mutms.com',
+	server: {
+		host: '0.0.0.0',
+		port: 4321
+	},
+
+	vite: {
+		server: {
+			allowedHosts: ['node.sitedocs.orb.local']
+		}
+	},
+});
